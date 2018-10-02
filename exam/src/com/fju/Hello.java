f974b70158391ce593be391c98832e37b0557ae9
user <user@es302n33>
2018-10-02 11:59:39 +0800

    exam

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,7 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		
+		int age = 19;
+		float height =1.78f;
+		int weight = 54;
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		float bmi = weight/(height*height);
+		System.out.println(bmi);
+		age = age + 1;
+		System.out.println(age);
 
 	}
 
